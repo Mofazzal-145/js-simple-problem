2f9e431827615d0ee0ec26d70d4ba408d1facc07
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-01 00:34:22 +0600

    math.js file added

---
+++ math.js
@@ -0,0 +1,43 @@
+
+// absolute numbar
+const myNumber = -5;
+const output = Math.abs(myNumber);
+// console.log(output);
+
+// celling number 
+const myNumber2 = 22.00001;
+const output2 = Math.ceil(myNumber2);
+// console.log(output2);
+
+// floor Number
+
+const myNumber3 = 22.75001;
+const output3 = Math.floor(myNumber3);
+// console.log(output3);
+
+// rounding Number
+
+const myNumber4 = 35.5;
+const output4 = Math.round(myNumber4);
+// console.log(output4);
+
+// random Number
+
+const output5 = Math.random();
+// console.log(output5);
+
+// ======random 2
+
+// const output6 = Math.random() * 10;
+// const rounded = Math.floor(output6);
+// console.log(rounded);
+
+// with loop
+
+for(let i = 0; i <= 20; i++){
+    // const output6 = Math.random() * 10;
+    // const rounded = Math.floor(output6);
+    const output6 = Math.random() * 6;
+    const rounded = Math.round(output6);
+    console.log(rounded);
+}
